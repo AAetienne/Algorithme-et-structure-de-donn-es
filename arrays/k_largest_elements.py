1afe7e9a65d592c89f3545f96de191ee4aefc15e
Etienne AHANDA <158361468+AAetienne@users.noreply.github.com>
2024-05-23 01:14:16 +0200

    exercice k largest elements OK (one try YESS)

---
+++ arrays/k_largest_elements.py
@@ -0,0 +1,35 @@
+# User function Template for python3
+
+class Solution:
+    # Function to return k largest elements from an array.
+    def kLargest(self, li, n, k):
+        # code here
+        result = []
+        temp_array = sorted(li)
+        print(temp_array)
+
+        """on parcourt la table trié en commençant par le dernier element
+        len(li) -1 jusqu'à l'element len(li) - k -1 par pas de -1"""
+        for i in range(len(li) - 1, len(li) - k - 1, -1):
+            result.append(temp_array[i])
+            #i -= 1
+
+        return result
+
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+t = int(input())
+for _ in range(t):
+    li = [int(x) for x in input().strip().split()]
+    n = li[0]
+    k = li[1]
+    li = [int(x) for x in input().strip().split()]
+    ob = Solution()
+    k_largest_list = ob.kLargest(li, n, k)
+
+    for element in k_largest_list:
+        print(element, end=' ')
+    print('')
+# } Driver Code Ends
